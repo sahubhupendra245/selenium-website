884fc3e3a5ddeaf9b0762f3629429165c4ca78b1
sahubhupendra245 <sahu.bhupendra245@gmail.com>
2023-12-13 15:35:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker build ./mysql/ -t db
-docker run -it -d -p 3306:3306 --name mysql db
+sudo docker build ./mysql/ -t db
+sudo docker run -it -d -p 3306:3306 --name mysql db
 
-docker build ./website/ -t web
-docker run -it -d -p 3001:80 --link mysql:db web
+sudo docker build ./website/ -t web
+sudo docker run -it -d -p 3001:80 --link mysql:db web
